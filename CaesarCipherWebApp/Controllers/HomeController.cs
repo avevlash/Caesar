ce7de2ec91c1015251828ff0a1197d559797c7a9
Андрей Евлаш <avevlash@gmail.com>
2019-04-11 00:01:45 +0300

    Add project files.

---
+++ CaesarCipherWebApp/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CaesarCipherWebApp.Models;
+using System.Text;
+
+namespace CaesarCipherWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            
+            return View(new CipherViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(CipherViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            model.TextIn = model.ActionType == "Зашифровать" ? Encipher(model.TextIn, model.Shift) : Decipher(model.TextIn, model.Shift);
+            ModelState.Clear();
+            return View(model);
+        }
+        public static char cipher(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+            {
+                return ch;
+            }
+            char d = char.IsUpper(ch) ? 'А' : 'а';
+            return (char)((((ch + key) - d) % 33) + d);
+
+
+        }
+        public static string Encipher(string input, int key)
+        {
+            StringBuilder output = new StringBuilder();
+
+            foreach (char ch in input)
+                output.Append(cipher(ch, key));
+
+            return output.ToString();
+        }
+
+        public static string Decipher(string input, int key)
+        {
+            return Encipher(input, 33 - key);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CaesarCipherWebApp/Models/CipherViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaesarCipherWebApp.Models
+{
+    public class CipherViewModel
+    {
+        [Required(ErrorMessage = "Введите исходный текст!")]
+        public string TextIn { get; set; }
+        public string TextOut { get; set; }
+        [Required(ErrorMessage = "Выберите значение сдвига!")]
+        public int Shift { get; set; }
+        [Required(ErrorMessage = "Выберите тип действия!")]
+        public string ActionType { get; set; }
+        public List<CipherAction> Actions { get; set; }
+        public CipherViewModel()
+        {
+           Actions = new List<CipherAction>()
+                    {
+                        new CipherAction()
+                            { ID = 1, Name = "Зашифровать" },
+                        new CipherAction()
+                            { ID = 2, Name = "Расшифровать" }
+                    };
+        }
+    }
+    public class CipherAction
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+}
